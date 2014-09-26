fefe982534c8071082562c76fddeb7fdb70a8749
DGoRdriguez <alejandrorodriguezperalta@gmail.com>
2014-09-25 22:46:58 -0500

    Inicio Portal Web GS
    Inicio Portal Web GS

---
+++ Web_Bk/contacto.php
@@ -0,0 +1,20 @@
+<?php
+$nombre = $_POST['nombre'];
+$email = $_POST['mail'];
+$mensaje = $_POST['mensaje'];
+$para = 'serviciosgroupsystem@gmail';
+$titulo = 'Contactanos Web Site Group System';
+$header = 'From: ' . $email;
+$msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Mensaje:\n $mensaje";
+  
+if ($_POST['submit']) {
+if (mail($para, $titulo, $msjCorreo, $header)) {
+echo "<script language='javascript'>
+alert('Mensaje enviado, Muchas Gracias.. Pronto te contactamos.');
+window.location.href = 'http://www.serviciosgroupsystem.com';
+</script>";
+} else {
+echo 'Falló el envio';
+}
+}
+?>
