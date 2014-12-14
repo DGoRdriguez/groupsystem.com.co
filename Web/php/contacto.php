c04c1b1517aa9005b1aac4ec45fe6545e34c6e5a
DGoRdriguez <alejandrorodriguezperalta@gmail.com>
2014-12-14 08:30:12 -0500

    Cambio Logo
    Cambio Logo

--- Web/php/contacto.php
+++ Web/php/contacto.php
@@ -1,20 +1,15 @@
 <?php
-$nombre = $_POST['nombre'];
-$email = $_POST['mail'];
+$nombre = $_POST['nombre'];$compania = $_POST['compania'];
+$email = $_POST['email'];$telefono = $_POST['telefono'];
 $mensaje = $_POST['mensaje'];
-$para = 'serviciosgroupsystem@gmail';
-$titulo = 'Contactanos Web Site Group System';
-$header = 'From: ' . $email;
-$msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Mensaje:\n $mensaje";
+$para = 'comunicaciones@groupsystem.com.co';
+$titulo = 'Nuevo Mensaje de Contactanos Web Site Group System';
+$tituloCte = 'Contactanos Web Site Group System';$header = 'From: ' . $para;$headerCte = 'From: ' . $email;
+$msjGS = "\nCompañia: $compania\nNombre Contacto: $nombre\nTeléfono: $telefono\nE-Mail: $email\nMensaje:\n$mensaje\n";$msjCte = "\nSrs.: $compania\nEstimado: $nombre\n\nHemos Recibido su solicitud, un asesor se comunicara lo mas pronto posible.\n\n\n\nCordialmente,\nGROUP SYSTEM S A S\nEmail. comunicaciones@groupsystem.com.co\nTel. 660 66 40\nCel. 313 898 78 58 – 301 785 84 00\n";
   
-if ($_POST['submit']) {
-if (mail($para, $titulo, $msjCorreo, $header)) {
-echo "<script language='javascript'>
-alert('Mensaje enviado, Muchas Gracias.. Pronto te contactamos.');
-window.location.href = 'http://www.serviciosgroupsystem.com';
-</script>";
+if (mail($para, $titulo, $msjGS, $header)) {
+		if (mail($email, $tituloCte, $msjCte, $headerCte)) {					echo "<script language='javascript'>					alert('Mensaje enviado, Muchas Gracias.. Pronto te contactamos.');					window.location.href = 'http://groupsystem.com.co/Web/index.html';					</script>";			} else {					echo "<script language='javascript'>					alert('El Mensaje no pudo ser  enviado, por favor indiquenos su necesidad al e-mail comunicaciones@groupsystem.com');					window.location.href = 'http://groupsystem.com.co/Web/contacto.html';					</script>";			}
 } else {
-echo 'Falló el envio';
-}
-}
+		echo "<script language='javascript'>		alert('El Mensaje no pudo ser  enviado, por favor indiquenos su necesidad al e-mail comunicaciones@groupsystem.com');		window.location.href = 'http://groupsystem.com.co/Web/contacto.html';		</script>";
+}
 ?>
